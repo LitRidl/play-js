b91ee43ecd1fd8304c9641384142b24eafb8a1c9
SunRidl <sunrdil@gmail.com>
2015-03-12 11:28:14 +0300

    Fixed LightSpeed formatting

--- LightSpeed/js/sound.js
+++ LightSpeed/js/sound.js
@@ -8,38 +8,37 @@ explode.volume = .5;
 
 var GameSound = new sound();
 
-function sound()
-{
-	this.mute = true;
-	
-	this.play = function(sound){
-		if (!this.mute){
-			switch(sound){
-			case "shoot":
-				//explode = new Audio("assets/explosion.wav");
-				//explode.volume = .05;
-				//shootaudio.play();
-				break;
-			case "LSstart":
-				lightspeedstartaudio.play();
-				break;
-			case "LSgo":
-				lightspeedaudio.play();
-				break;
-			case "explode":
-				explode = new Audio("assets/explosion.wav");
-				explode.volume = .05;
-				explode.play();
-				break;
-			case "playerexplode":
-				break;
-			case "playerhit":
-				break;
-			case "levelup":
-				break;
-			case "lightspeedready":
-				break;
-			}
-		}
-	}
+function sound() {
+    this.mute = true;
+
+    this.play = function (sound) {
+        if (!this.mute) {
+            switch (sound) {
+                case "shoot":
+                    //explode = new Audio("assets/explosion.wav");
+                    //explode.volume = .05;
+                    //shootaudio.play();
+                    break;
+                case "LSstart":
+                    lightspeedstartaudio.play();
+                    break;
+                case "LSgo":
+                    lightspeedaudio.play();
+                    break;
+                case "explode":
+                    explode = new Audio("assets/explosion.wav");
+                    explode.volume = .05;
+                    explode.play();
+                    break;
+                case "playerexplode":
+                    break;
+                case "playerhit":
+                    break;
+                case "levelup":
+                    break;
+                case "lightspeedready":
+                    break;
+            }
+        }
+    }
 }
